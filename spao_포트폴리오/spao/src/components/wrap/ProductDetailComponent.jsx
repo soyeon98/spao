11416505a9c72403fe2b1b0d8b74e89c86512150
soyeon98 <212psy@naver.com>
2023-07-21 16:32:25 +0900

    0721

--- spao_포트폴리오/spao/src/components/wrap/ProductDetailComponent.jsx
+++ spao_포트폴리오/spao/src/components/wrap/ProductDetailComponent.jsx
@@ -1,11 +1,7 @@
 import React from 'react';
-// import { useParams } from 'react-router-dom';
 
 export default function ProductDetailComponent ({setViewProduct}){
 
-
-
-    // const {id} = useParams();
 
     const [state,setState]=React.useState({
         product:{},
