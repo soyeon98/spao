e3d40cf2aa6fe470296355a81695cb610d867ec5
soyeon98 <212psy@naver.com>
2023-12-20 01:01:15 +0900

    recent front

--- spao_포트폴리오/spao/src/components/wrap/mypage/RecentComponent.jsx
+++ spao_포트폴리오/spao/src/components/wrap/mypage/RecentComponent.jsx
@@ -27,7 +27,7 @@ export default function RecentComponent() {
                                     <td><a href="!#"><img src="./img/mypage/02e2a016b14273735e1b4c108b82d31e.jpg" alt="" /></a></td>
                                     <td><a href="!#">크롭 부클 배색 반팔니트_SPKWD25W14</a></td>
                                     <td>
-                                        <span>Size:</span>
+                                        <span>Size :</span>
                                         <select name="size" id="size">
                                             <option value="">- [필수] 옵션을 선택해 주세요 -</option>
                                             <option value="S(085)">S(085)</option>
@@ -39,7 +39,30 @@ export default function RecentComponent() {
                                         <div className="origin">39,900</div>
                                         <div className="sale">29,900</div>
                                     </td>
-                                    <td><span><img src="./img/mypage/ico_product_point.gif" alt="" /></span>299원 (1%)</td>
+                                    <td><span className='point'><img src="./img/mypage/ico_product_point.gif" alt="" />299원 (1%)</span></td>
+                                    <td>
+                                        <button>담기</button>
+                                        <button>주문</button>
+                                        <button>삭제</button>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td><a href="!#"><img src="./img/mypage/02e2a016b14273735e1b4c108b82d31e.jpg" alt="" /></a></td>
+                                    <td><a href="!#">크롭 부클 배색 반팔니트_SPKWD25W14</a></td>
+                                    <td>
+                                        <span>Size :</span>
+                                        <select name="size" id="size">
+                                            <option value="">- [필수] 옵션을 선택해 주세요 -</option>
+                                            <option value="S(085)">S(085)</option>
+                                            <option value="M(090)">M(090)</option>
+                                            <option value="L(095)">L(095)</option>
+                                        </select>
+                                    </td>
+                                    <td>
+                                        <div className="origin">39,900</div>
+                                        <div className="sale">29,900</div>
+                                    </td>
+                                    <td><span className='point'><img src="./img/mypage/ico_product_point.gif" alt="" />299원 (1%)</span></td>
                                     <td>
                                         <button>담기</button>
                                         <button>주문</button>
